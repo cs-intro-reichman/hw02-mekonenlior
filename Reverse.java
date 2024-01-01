788defa4e3e289c96d9dcd1d719bd8b19e9763cc
Lior Mekonen <lior.mekonen01@post.runi.ac.il>
2024-01-01 18:38:19 +0200

    reverse

--- Reverse.java
+++ Reverse.java
@@ -5,15 +5,16 @@
 public class Reverse {
 	public static void main (String[] args){
 		String word = args[0];
-		String reversedWord = "";
-		for (int i = word.length() - 1; i >= 0; i--) {
-			reversedWord += word.charAt(i);
-		}
-		int middleIndex = word.length() / 2;
-		char middleChar = word.charAt(middleIndex);
+        String reversedWord = "";
 
-		System.out.println(reversedWord);
-		System.out.println("The middle character is " + middleChar);
-		System.out.println("yes");
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
+        int middleIndex = word.length() / 2;
+        char middleChar = word.charAt(middleIndex);
+
+        System.out.println(reversedWord);
+        System.out.println("The middle character is " + middleChar);
 	}
 }
