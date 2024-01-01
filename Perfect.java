655ef135facbf7865e186a606a2f3dad8b07c470
Lior Mekonen <lior.mekonen01@post.runi.ac.il>
2024-01-01 17:53:52 +0200

    modified perfect and reverse

--- Perfect.java
+++ Perfect.java
@@ -4,18 +4,18 @@
 public class Perfect {
 	public static void main (String[] args) {
 		int N = Integer.parseInt(args[0]);
-        int sumOfDivisors = 0;
+        int sumOfDivisors = 1; // Include 1 as a divisor
         String divisors = "";
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 2; i < N; i++) {  // Start from 2 to avoid duplicates
             if (N % i == 0) {
                 sumOfDivisors += i;
-                divisors += i + " + ";
+                divisors += " + " + i;
             }
         }
 
         if (sumOfDivisors == N) {
-            System.out.println(N + " is a perfect number since " + N + " = " + divisors);
+            System.out.println(N + " is a perfect number since " + N + " = 1" + divisors);
         } else {
             System.out.println(N + " is not a perfect number");
         }
