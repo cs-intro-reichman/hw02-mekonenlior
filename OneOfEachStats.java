7e57a271804c98761b336bc848eee885cae07f47
Lior Mekonen <lior.mekonen01@post.runi.ac.il>
2024-01-02 19:59:45 +0200

    ooes completed?

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -9,22 +9,23 @@ import java.util.Random;
  */
 public class OneOfEachStats {
 	public static void main (String[] args) {
-		// Gets the two command-line arguments
+
         int T = Integer.parseInt(args[0]);
         int seed = Integer.parseInt(args[1]);
         Random generator = new Random(seed);
-        // Initailizes a random numbers generator with the given seed value 
+
         int totalChildren = 0;
         int familiesWith2 = 0;
         int familiesWith3 = 0;
         int familiesWith4OrMore = 0;
+        int mostCommonCount = 0;
+        int mostCommonNumberOfChildren = 0;
 
         for (int i = 1; i <= T; i++) {
             int sumOfChildren = 0;    
             int boys = 0;           
             int girls = 0;          
             do {
-                
                 if (generator.nextDouble() <= 0.5) {
                     boys++;
                 } else {
@@ -35,20 +36,30 @@ public class OneOfEachStats {
             totalChildren += sumOfChildren;
             if (sumOfChildren == 2) {
                 familiesWith2++;
+                if (familiesWith2 > mostCommonCount) {
+                    mostCommonCount = familiesWith2;
+                    mostCommonNumberOfChildren = 2;
+                }
             } else if (sumOfChildren == 3) {
                 familiesWith3++;
+                if (familiesWith3 > mostCommonCount) {
+                    mostCommonCount = familiesWith3;
+                    mostCommonNumberOfChildren = 3;
+                }
             } else {
                 familiesWith4OrMore++;
+                if (familiesWith4OrMore > mostCommonCount) {
+                    mostCommonCount = familiesWith4OrMore;
+                    mostCommonNumberOfChildren = 4; // Or more, as needed
+                }
             }
         }
-
+        
         double average = (double) totalChildren / T;
-        int mostCommon = Math.max(Math.max(familiesWith2, familiesWith3), familiesWith4OrMore);
-
         System.out.println("Average: " + average + " children to get at least one of each gender.");
         System.out.println("Number of families with 2 children: " + familiesWith2);
         System.out.println("Number of families with 3 children: " + familiesWith3);
         System.out.println("Number of families with 4 children or more: " + familiesWith4OrMore);
-        System.out.println("The most common number of children is " + mostCommon);    
+        System.out.println("The most common number of children is " + mostCommonNumberOfChildren);    
 	}
 }
